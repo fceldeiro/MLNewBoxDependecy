bfd3ac566c1dd73a45e8161a775e8a36915146e8
Fabian Celdeiro <fabian.celdeiro@mercadolibre.com>
2014-09-06 12:02:43 -0300

    Initial commit

---
+++ Example/MLNewBoxDependecy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MLNewBoxDependecy
+//
+//  Created by Fabian Celdeiro on 09/06/2014.
+//  Copyright (c) 2014 Fabian Celdeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLAppDelegate class]));
+    }
+}
